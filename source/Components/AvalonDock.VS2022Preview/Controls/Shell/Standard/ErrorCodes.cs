cc17bf39d01db0769c02df711e9386188428a579
Daniel Banda <danielbanda@microsoft.com>
2025-03-09 22:05:45 -0700

    Revert "Warnings"
    This reverts commit b887663f

--- source/Components/AvalonDock.VS2022Preview/Controls/Shell/Standard/ErrorCodes.cs
+++ source/Components/AvalonDock.VS2022Preview/Controls/Shell/Standard/ErrorCodes.cs
@@ -482,7 +482,6 @@ namespace Standard
 		/// <summary>
 		/// Convert the result of Win32 GetLastError() into a raised exception.
 		/// </summary>
-		[DoesNotReturn]
 		public static void ThrowLastError()
 		{
 			((HRESULT)Win32Error.GetLastError()).ThrowIfFailed();
